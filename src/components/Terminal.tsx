5018b714884747f3d2905264696a20e61fd9b548
Jahaad Petty <jp@Jahaads-MacBook-Air.local>
2025-04-30 14:36:26 -0400

    ZE SCREEN; ZIS BLACK ! ( background styles applied, terminal component created and rendered on app.js luh twin )

---
+++ src/components/Terminal.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Terminal.css";
+
+const Terminal: React.FC = () => {
+  return (
+    <div className="terminal">
+      {/* Terminal content will go here */}
+    </div>
+  );
+};
+
+export default Terminal;
