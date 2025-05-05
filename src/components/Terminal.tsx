d613acc32245d05badc5304e0bdaeeb3239d2702
Jahaad Petty <jp@Jahaads-MacBook-Air.local>
2025-05-05 09:55:27 -0400

    feat: Implement basic Header component with optional name and username props

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./NavBar";
 import "./Terminal.css";
+import Header from "./Header";
 
 const Terminal: React.FC = () => {
   return (
     <div className="terminal">
         <NavBar/>
+        <Header/>
     </div>
   );
 };
