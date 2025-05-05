95f30a1674aaf650038db24e85d53bcbe4d84767
Jahaad Petty <jp@Jahaads-MacBook-Air.local>
2025-05-05 11:51:18 -0400

    feat: add Ruler component to dynamically calculate char limit based on screen width

---
+++ src/components/Ruler.tsx
@@ -0,0 +1,29 @@
+import React, {useRef, useEffect} from "react";
+
+export type RulerProps = {
+    onMeasure: (charLimit: number) => void;
+};
+
+
+const Ruler: React.FC<RulerProps> = ({onMeasure}) => {
+    const spanRef = useRef<HTMLSpanElement>(null);
+    let charCount = 1000
+
+    useEffect(() => {
+        if (!spanRef.current) return
+        const width = spanRef.current.offsetWidth;
+        const charWidth = width / charCount;
+        const containerWidth = window.innerWidth
+        const charLimit = Math.floor(containerWidth / charWidth)
+        console.log(`Span Width: ${spanRef.current.offsetWidth}px, Char Width: ${(spanRef.current.offsetWidth / charCount).toFixed(2)}px, Container Width: ${window.innerWidth}px, Char Limit: ${Math.floor(window.innerWidth / (spanRef.current.offsetWidth / charCount))}`);
+        onMeasure(charLimit)
+    }, [])
+  return (
+    <div className="ruler">
+        <span ref={spanRef}>{'M'.repeat(charCount)}</span>
+    </div>
+  );
+};
+
+export default Ruler;
+
--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import NavBar from "./NavBar";
 import "./Terminal.css";
 import Header from "./Header";
+import Line from "./Line";
+import Ruler from "./Ruler";
+
 
 const Terminal: React.FC = () => {
+  const [charLimit, setCharLimit] = useState<number | null>(null)
+
   return (
     <div className="terminal">
-        <NavBar/>
-        <Header/>
+      <Ruler onMeasure={setCharLimit}/>
+      
+      <NavBar/>
+      <Header/>
+      <Line/>
+      <Line/>
+
+
     </div>
   );
 };
